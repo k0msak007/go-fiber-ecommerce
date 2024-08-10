e1a9b9f25faac6f7185eb0fb5a4834b01858b987
komsak007 <98792390+k0msak007@users.noreply.github.com>
2024-08-10 10:22:06 +0700

    add config map env file

--- config/config.go
+++ config/config.go
@@ -1,1 +1,94 @@
 package config
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+)
+
+func envPath() string {
+	if len(os.Args) == 1 {
+		return ".env"
+	} else {
+		return os.Args[1]
+	}
+}
+
+func LoadConfig(path string) IConfig {
+	envMap, err := godotenv.Read(path)
+	if err != nil {
+		log.Fatalf("load dotenv failed: %v", err)
+	}
+
+	return &config{
+		app: &app{},
+		db:  &db{},
+		jwt: &jwt{},
+	}
+}
+
+type IConfig interface {
+	App() IAppConfig
+	Db() IDbConfig
+	Jwt() IJwtConfig
+}
+
+type config struct {
+	app *app
+	db  *db
+	jwt *jwt
+}
+
+type IAppConfig interface {
+}
+
+type app struct {
+	host         string
+	port         uint
+	name         string
+	version      string
+	readTimeout  time.Duration
+	writeTimeout time.Duration
+	bodyLimit    int
+	fileLimit    int
+	gcpbucket    string
+}
+
+func (c *config) App() IAppConfig {
+	return nil
+}
+
+type IDbConfig interface {
+}
+
+type db struct {
+	host           string
+	port           int
+	protocol       string
+	username       string
+	password       string
+	database       string
+	sslMode        string
+	maxConnections int
+}
+
+func (c *config) Db() IDbConfig {
+	return nil
+}
+
+type IJwtConfig interface {
+}
+
+type jwt struct {
+	adminKey         string
+	secretKey        string
+	apiKey           string
+	accessExpiresAt  time.Duration
+	refreshExpiresAt time.Duration
+}
+
+func (c *config) Jwt() IJwtConfig {
+	return nil
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/k0msak007/go-fiber-ecommerce
 
 go 1.22.5
+
+require github.com/joho/godotenv v1.5.1 // indirect
